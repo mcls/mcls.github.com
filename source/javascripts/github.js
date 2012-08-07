eb1bec3d3c147be6f18c198ded8bee69ed3f9511
Maarten Claes <maartencls@gmail.com>
2012-08-07 23:20:45 +0200

    Update github.js for v3 of the API

--- source/javascripts/github.js
+++ source/javascripts/github.js
@@ -7,30 +7,41 @@ var github = (function(){
     }
     t.innerHTML = fragment;
   }
+
+  function sortReposByPushedAt(repos) {
+    repos.sort(function(a, b) {
+      var aDate = new Date(a.pushed_at).valueOf(),
+          bDate = new Date(b.pushed_at).valueOf();
+
+      if (aDate === bDate) { return 0; }
+      return aDate > bDate ? -1 : 1;
+    });
+  }
+
+  function filterForks(repos) {
+    var notForked = [];
+    for (var i = 0; i < repos.length; i++) {
+      if (repos[i].fork) { continue; }
+      notForked.push(repos[i]);
+    }
+    return notForked;
+  }
+
   return {
     showRepos: function(options){
       $.ajax({
-          url: "http://github.com/api/v2/json/repos/show/"+options.user+"?callback=?"
+          url: "https://api.github.com/users/" + options.user + "/repos?callback=?"
         , type: 'jsonp'
         , error: function (err) { $(options.target + ' li.loading').addClass('error').text("Error loading feed"); }
         , success: function(data) {
-          var repos = [];
-          if (!data || !data.repositories) { return; }
-          for (var i = 0; i < data.repositories.length; i++) {
-            if (options.skip_forks && data.repositories[i].fork) { continue; }
-            repos.push(data.repositories[i]);
+            var repos = data.data;
+            if (!repos) { return; }
+            if (options.skip_forks) { repos = filterForks(repos); }
+            sortReposByPushedAt(repos);
+
+            if (options.count) { repos.splice(options.count); }
+            render(options.target, repos);
           }
-          repos.sort(function(a, b) {
-            var aDate = new Date(a.pushed_at).valueOf(),
-                bDate = new Date(b.pushed_at).valueOf();
-
-            if (aDate === bDate) { return 0; }
-            return aDate > bDate ? -1 : 1;
-          });
-
-          if (options.count) { repos.splice(options.count); }
-          render(options.target, repos);
-        }
       });
     }
   };
